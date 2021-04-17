572ab69e2895da1f0877db104b7602a09ec65609
Jappanjeet Singh <jappanjeet.99@gmail.com>
2021-04-17 17:08:06 +0530

    fixing logic in switching page to tracking

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1b7d83b6363db4fc8e0e";
+/******/ 	var hotCurrentHash = "f932f8114bb8cc339800";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
