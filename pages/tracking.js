04e41ce63f18397175788a51def2cce7059b5c55
Jappanjeet Singh <jappanjeet.99@gmail.com>
2021-04-17 13:43:12 +0530

    changing styles

--- pages/tracking.js
+++ pages/tracking.js
@@ -14,7 +14,7 @@ const Tracker = () => {
         setPageLoading(true)
         setTimeout(() => {
             setPageLoading(false)
-        }, 10000);
+        }, 5000);
     }, [])
 
     const SEO = {
@@ -39,7 +39,7 @@ const Tracker = () => {
                     </div>
                 )
                 : null}
-            {/* <Tracking /> */}
+            <Tracking />
         </>
     )
 }
